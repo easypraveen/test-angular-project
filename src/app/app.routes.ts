f17ea237960688858b0462e869000e2c6a1e59ee
easypraveen <praveensinghrathore03@gmail.com>
2025-06-12 18:47:30 +0530

    latest version code

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
-import { ProfileComponent } from './pages/profile/profile.component';
+import { UserListComponent } from './pages/user/user-list/user-list.component';
+import { userSelectedGuard } from './guards/user-selected.guard';
 
-export const routes: Routes = [{path: '', component: HomeComponent},
-    {path : 'profile', component: ProfileComponent},
+export const routes: Routes = [
+    {path: '', component: HomeComponent},
+    {path : 'users', component : UserListComponent},
+    {path : 'user/:id', loadComponent: () => import('./pages/user/user-detail/user-detail.component')
+        .then(m => m.UserDetailComponent) , canActivate : [userSelectedGuard]},
     {path: '**', redirectTo: ''}
 ];
